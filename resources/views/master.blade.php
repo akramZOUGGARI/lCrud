9a0488e75198cf112db4cf58654816aedb880e37
akram <zakram.klf@gmail.com>
2017-05-02 15:09:53 -0400

    new view

---
+++ resources/views/master.blade.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>lCrud</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href=" {{ asset('/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href=" {{ asset('/css/font-awesome.min.css') }}">
+
+ 
+</head>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+</div>	
+
+
+  <script src=" {{ asset('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js') }}"></script>
+  <script src=" {{ asset('/bootstrap.min.js') }}"></script>
+
+<body>
+
+</body>
+</html>
